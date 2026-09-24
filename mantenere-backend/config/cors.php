e0fa359d8cb4bad6b4beb0c87dc9aea7ea5abd78
KennyDzul <163344883+KennyDzul@users.noreply.github.com>
2026-09-24 14:26:29 -0600

    funciona

--- mantenere-backend/config/cors.php
+++ mantenere-backend/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie', 'cotizaciones/generar-pdf/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
